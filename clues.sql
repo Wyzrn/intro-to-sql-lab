38ffcb891cf57e1aa67b78b56ee2555e19b1fa8c
Bastian Pethel <bastianpethel360@gmail.com>
2025-09-22 16:19:42 -0400

    fixed logic to get correct answers

--- clues.sql
+++ clues.sql
@@ -1,34 +1,67 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
 -- Write SQL query here
-select name, population, continent from countries where continent = 'Europe' order by population asc limit 1;
+SELECT name, population FROM countries 
+WHERE region = 'Southern Europe' 
+ORDER BY population ASC 
+LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
-SELECT language FROM countrylanguages WHERE countrycode = 'VAT' AND isofficial = true LIMIT 1;
+SELECT language FROM countrylanguages 
+WHERE countrycode = 'VAT' 
+AND isofficial = true 
+LIMIT 1;
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-SELECT name FROM countries WHERE code IN (SELECT countrycode FROM countrylanguages WHERE language = 'Italian' GROUP BY countrycode HAVING COUNT(*) = 1) AND code != 'VAT' LIMIT 1;
+SELECT DISTINCT c.name 
+FROM countries c
+WHERE c.code IN (
+    SELECT cl.countrycode 
+    FROM countrylanguages cl 
+    WHERE cl.countrycode IN (
+        SELECT countrycode 
+        FROM countrylanguages 
+        WHERE language = 'Italian'
+    )
+    GROUP BY cl.countrycode
+    HAVING COUNT(DISTINCT cl.language) = 1
+) 
+AND c.code != 'VAT';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
-SELECT name FROM cities WHERE countrycode = 'ITA' AND name != 'Italy' AND id != (SELECT capital FROM countries WHERE code = 'ITA') LIMIT 1;
+SELECT name FROM cities 
+WHERE countrycode = 'SMR' 
+AND name != 'San Marino' 
+AND id != (SELECT capital FROM countries WHERE code = 'SMR') 
+ORDER BY population DESC
+LIMIT 1;
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-SELECT cities.name FROM cities JOIN countries ON cities.countrycode = countries.code WHERE countries.continent = 'South America' AND cities.name LIKE 'Mil%' AND cities.name != 'Milan' LIMIT 1;
+SELECT cities.name, countries.name as country
+FROM cities 
+JOIN countries ON cities.countrycode = countries.code 
+WHERE countries.continent = 'South America' 
+AND cities.name LIKE 'Serra%'
+AND cities.name != 'Serravalle'
+LIMIT 1;
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
-SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE code = 'ECU') LIMIT 1;
+SELECT cities.name, cities.population, countries.name as country
+FROM cities 
+JOIN countries ON cities.countrycode = countries.code 
+WHERE cities.population = 91085;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -43,6 +76,7 @@ SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE code = '
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-SELECT cities.name FROM cities JOIN countries ON cities.countrycode = countries.code WHERE cities.population BETWEEN 90000 AND 92000 LIMIT 1;
-
-Tandil
+SELECT cities.name, cities.population, countries.name as country
+FROM cities 
+JOIN countries ON cities.countrycode = countries.code 
+WHERE cities.population = 91085;
